cyh7789/protobuf

Support assigning repeated scalar fields using Python Buffer API.
PiperOrigin-RevId: 918392317

python/build_targets.bzl
@@ -144,6 +144,7 @@ def build_targets(name):
             "@abseil-cpp//absl/base:core_headers",
             "@abseil-cpp//absl/base:no_destructor",
             "@abseil-cpp//absl/container:flat_hash_map",
+            "@abseil-cpp//absl/functional:function_ref",
             "@abseil-cpp//absl/log:absl_check",
             "@abseil-cpp//absl/log:absl_log",
             "@abseil-cpp//absl/status",

Dustin4444/protobuf-1

python/build_targets.bzl
@@ -159,6 +159,7 @@ def build_targets(name):
             "@abseil-cpp//absl/status:statusor",
             "@abseil-cpp//absl/strings",
             "@abseil-cpp//absl/synchronization",
+            "@abseil-cpp//absl/types:span",
             "@system_python//:python_headers",
         ],
     )